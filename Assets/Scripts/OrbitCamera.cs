06db74e21ec504e528ad7d42403c6c74c65b7969
Raymond L Maple <rmaple@neumont.edu>
2024-01-16 11:49:22 -0700

    class 011624

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class OrbitCamera : MonoBehaviour
 {
-    [SerializeField] Transform target = null;
-    [SerializeField][Range(20, 90)] float pitch = 40;
-    [SerializeField][Range(2, 8)] float distance = 5;
-    [SerializeField][Range(0.1f, 2.0f)] float sensitivity = 1;
+	[SerializeField] Transform target = null;
+	[SerializeField][Range(20, 90)] float defaultPitch = 40;
+	[SerializeField][Range(2, 8)] float distance = 5;
+	[SerializeField][Range(0.1f, 2.0f)] float sensitivity = 1;
 
-    float yaw = 0;
+	float yaw = 0;
+	float pitch = 0;
 
-    void Update()
-    {
-        yaw += Input.GetAxis("Mouse X") * sensitivity;
+	private void Start()
+	{
+		pitch = defaultPitch;
+	}
 
-        Quaternion qyaw = Quaternion.AngleAxis(yaw, Vector3.up);
-        Quaternion qpitch = Quaternion.AngleAxis(pitch, Vector3.right);
-        Quaternion rotation = qyaw * qpitch;
+	void Update()
+	{
+		yaw += Input.GetAxis("Mouse X") * sensitivity;
+		pitch += Input.GetAxis("Mouse Y") * sensitivity;
+		pitch = Mathf.Clamp(pitch, 40.0f, 70.0f);
 
-        transform.position = target.position + (rotation * Vector3.back * distance);
-        transform.rotation = rotation;
-    }
+		Quaternion qyaw = Quaternion.AngleAxis(yaw, Vector3.up);
+		Quaternion qpitch = Quaternion.AngleAxis(pitch, Vector3.right);
+		Quaternion rotation = qyaw * qpitch;
+
+		transform.position = target.position + (rotation * Vector3.back * distance);
+		transform.rotation = rotation;
+	}
 }
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+	[SerializeField] Animator animator;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		animator.SetTrigger("Start");
+	}
+
+
+}
