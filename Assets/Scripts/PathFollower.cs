840ad1b947db19e8d5eac6238ab7835415744ee9
Raymond L Maple <rmaple@neumont.edu>
2024-01-25 17:58:50 -0700

    class 012524

---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+using Unity.Mathematics;
+
+public class PathFollower : MonoBehaviour
+{
+	[SerializeField] SplineContainer splineContainer;
+	[Range(0, 40)] public float speed = 1;
+	[Range(0, 1)] public float tdistance = 0; // distance along spline (0-1)
+
+	// length in world coordinates
+	public float length { get { return splineContainer.CalculateLength(); } }
+	// distance in world coordinates
+	public float distance
+	{
+		get { return tdistance * length; }
+		set { tdistance = value / length; }
+	}
+
+	void Update()
+	{
+		distance += speed * Time.deltaTime;
+		UpdateTransform(math.frac(tdistance));
+	}
+
+	void UpdateTransform(float t)
+	{
+		Vector3 position = splineContainer.EvaluatePosition(t);
+		Vector3 up = splineContainer.EvaluateUpVector(t);
+		Vector3 forward = Vector3.Normalize(splineContainer.EvaluateTangent(t));
+		Vector3 right = Vector3.Cross(up, forward);
+
+		transform.position = position;
+		transform.rotation = Quaternion.LookRotation(forward, up);
+	}
+}
